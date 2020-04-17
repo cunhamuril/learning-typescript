88c1f39735ba677ee80288eb3d291cf9ba800bd4
Murilo <murilo.sant@hotmail.com>
2020-04-16 23:13:15 -0300

    feat(app): integrate api

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+import api from "./services/api";
+
+interface IUser {
+  name: string;
+  email: string;
+}
 
 function App() {
+  const [users, setUsers] = useState<IUser[]>([]);
+
+  useEffect(() => {
+    api.get<IUser[]>("/users").then((res) => {
+      setUsers(res.data);
+    });
+  }, []);
+
   return (
     <div className="App">
-      <h1>Hello TypeScript</h1>
+      {users.map((user) => (
+        <p>{user.name}</p>
+      ))}
     </div>
   );
 }
