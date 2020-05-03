e82fdbe23b96d0dd70117305ad4319b6332ca032
Murilo Cunha <murilo.sant@hotmail.com>
2020-05-03 13:05:14 -0300

    feat(view): added jquery

--- alura/alurabank/src/ts/views/View.ts
+++ alura/alurabank/src/ts/views/View.ts
@@ -1,3 +1,5 @@
+declare var $: any;
+
 // <T>: classe tipo genérico
 abstract class View<T> {
   /**
@@ -5,14 +7,15 @@ abstract class View<T> {
    * Podemos resolver esse erro de compilação relaxando um pouco o nível de acesso à propriedade,
    * utilizando o modificador protected em vez de private:
    */
-  protected _element: Element;
+  // protected _element: Element;
+  protected _element: any; // com jQuery
 
   constructor(selector: string) {
-    this._element = document.querySelector(selector);
+    this._element = $(selector);
   }
 
   update(model: T) {
-    this._element.innerHTML = this.template(model);
+    this._element.html(this.template(model));
   }
 
   // Abstract method: só tem assinatura, não tem corpo. Só é utilizando por quem herdar
